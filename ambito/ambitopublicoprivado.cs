f714b9a30b185482fed76192f55c5455653e8d99
amaiasanchis <amaiasanchisb@gmail.com>
2022-05-31 18:44:09 +0200

    primeros ejercicios

---
+++ ambito/ambitopublicoprivado.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MyNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "Microsoft Learn";
+            string reversedValue = Utility.Reverse(value);
+            Console.WriteLine($"Secret message: {reversedValue}");
+        }
+    }
+
+    class Utility
+    {
+        public static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
